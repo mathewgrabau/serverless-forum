221ad591ebca37f9cd35ae9abce0764b3f719624
Mathew Grabau <mgrabau@mathewgrabau.ca>
2018-02-05 16:04:40 -0600

    Successfully implemented and deployed function to the AWS

--- lambda-test/src/main/java/com/serverlessarchitecture/lambda/test/Handler.java
+++ lambda-test/src/main/java/com/serverlessarchitecture/lambda/test/Handler.java
@@ -1,5 +1,21 @@
 package com.serverlessarchitecture.lambda.test;
 
-public class Handler {
-    
+import com.amazonaws.services.lambda.runtime.Context;
+import com.serverlessarchitecture.lambda.LambdaHandler;
+
+public class Handler extends LambdaHandler<Handler.TestInput, Handler.TestOutput> {
+    static class TestInput {
+        public String value;
+    }
+
+    static class TestOutput {
+        public String value;
+    }
+
+    @Override
+    public TestOutput handleRequest(TestInput input, Context context) {
+        TestOutput testOutput = new TestOutput();
+        testOutput.value = input.value;
+        return testOutput;
+    }
 }
